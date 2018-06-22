8fa8d9aeaf674e9c2294fc72516a8090cf3fab40
Martynas <marcius117@gmail.com>
2018-06-22 13:35:26 +0300

    js exercise - How long You are living on earth

--- JAVASCRIPT/js/scritp.js
+++ JAVASCRIPT/js/scritp.js
@@ -1,7 +1,28 @@
-var name = prompt( "what is your name");
-var lastName = prompt( "what is your last name");
-var age = prompt("what is your age");
-var fullName = +name+ " " +lastName
+// ------======  first exercise ====-----=-=
+// ------======  Full name and age exercise in console.log ====-----=-=
 
-console.log( "Your full name is " +fullName);
-console.log( "You are " +age +" years old");
+// var name = prompt( "what is your name");
+// var lastName = prompt( "what is your last name");
+// var age = prompt("what is your age");
+// var fullName = +name+ " " +lastName
+
+// console.log( "Your full name is " +fullName);
+// console.log( "You are " +age +" years old");
+
+// ------======  secand exercise ====-----=-=
+
+// ------======  How long are You living on earth ====-----=-=
+
+var age = prompt("What is your age?");
+var yourDaysOnErth = age * 365;
+
+alert( "You have been living for roughly " +yourDaysOnErth+ " days on Earth");
+
+var husbandAge = prompt("What is your's husbund age?");
+var YourHusbandAgeOnEarth = husbandAge * 365;
+var youAreOlder = yourDaysOnErth - YourHusbandAgeOnEarth;
+
+alert("You are living " +youAreOlder+ " days longer then your husband on Earth");
+alert("CONGRUDALATIONS - You are smarter then Your husband");
+alert("Don't laught - A am a computer, I am smarter then You");
+alert("cha cha. It was a joke, dont smash me. Please");
