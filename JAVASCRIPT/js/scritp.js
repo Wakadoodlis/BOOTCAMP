48013a0e470baab7d58d248c56e69000ba94c654
Martynas <marcius117@gmail.com>
2018-06-22 12:34:36 +0300

    js exercise

---
+++ JAVASCRIPT/js/scritp.js
@@ -0,0 +1,7 @@
+var name = prompt( "what is your name");
+var lastName = prompt( "what is your last name");
+var age = prompt("what is your age");
+var fullName = +name+ " " +lastName
+
+console.log( "Your full name is " +fullName);
+console.log( "You are " +age +" years old");
