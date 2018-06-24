08d2bd31adbd831a54de4e8e543528ba39b85484
Martynas <marcius117@gmail.com>
2018-06-24 20:32:23 +0300

    new

--- JAVASCRIPT/js/scritp.js
+++ JAVASCRIPT/js/scritp.js
@@ -13,28 +13,31 @@
 
 // ------======  How long are You living on earth ====-----=-=
 
-var name = prompt( "Hello, my name is Mac. What is your name ?");
-var age = prompt("Nice to meet You " +name+ ". "+ "Could I ask You, what is Your age?");
-var yourDaysOnErth = age * 365;
+// var name = prompt( "Hello, my name is Mac. What is your name ?");
+// var age = prompt("Nice to meet You " +name+ ". "+ "Could I ask You, what is Your age?");
+// var yourDaysOnErth = age * 365;
 
-alert( "oh, You are so young! You are living on earth just roughly " +yourDaysOnErth+ " days.");
+// alert( "oh, You are so young! You are living on earth just roughly " +yourDaysOnErth+ " days.");
 
-var married = prompt("Are You single or married?")
-var husbandName =  prompt("oh what a shame... What is Your husband's name then? ")
+// var married = prompt("Are You single or married?")
+// var husbandName =  prompt("oh what a shame... What is Your husband's name then? ")
 
-var husbandAge = prompt("What is " +husbandName+ " " + "age?");
-var YourHusbandAgeOnEarth = husbandAge * 365;
-var youAreOlder = yourDaysOnErth - YourHusbandAgeOnEarth;
+// var husbandAge = prompt("What is " +husbandName+ " " + "age?");
+// var YourHusbandAgeOnEarth = husbandAge * 365;
+// var youAreOlder = yourDaysOnErth - YourHusbandAgeOnEarth;
 
-alert("Perdon? so You are older then " +husbandName+ " " +"for " +youAreOlder+ " days...");
+// alert("Perdon? so You are older then " +husbandName+ " " +"for " +youAreOlder+ " days...");
 
-alert(" and because You are older - You are also SMARTER,CONGRUTALATIONS !! :) ");
+// alert(" and because You are older - You are also SMARTER,CONGRUTALATIONS !! :) ");
 
-alert("You know what " +name+ " ?" +" I made some looking into inthernet while we were talking, and I found You and " +husbandName+ ".");
+// alert("You know what " +name+ " ?" +" I made some looking into inthernet while we were talking, and I found You and " +husbandName+ ".");
 
-prompt( "You two are very beautiful couple. Do You love him ?");
+// prompt( "You two are very beautiful couple. Do You love him ?");
 
 
-alert(" oh shout, look what the time. I have to go. It was nice to talk with You " +name+ " ! See You next time. ")
+// alert(" oh shout, look what the time. I have to go. It was nice to talk with You " +name+ " ! See You next time. ")
 
-alert(" By the way... I think He loves You too :) Just look at You twoo! You love birds... :) ok, its time for me to go...Bye ;) p.s. this photo I found was in the 'couples for ever' photo section of the whole inthernet ;) see by Yourself, just press 'ok' ;) ");
+// alert(" By the way... I think He loves You too :) Just look at You twoo! You love birds... :) ok, its time for me to go...Bye ;) p.s. this photo I found was in the 'couples for ever' photo section of the whole inthernet ;) see by Yourself, just press 'ok' ;) ");
+
+// ------======  new assigment ====-----=-=
+
