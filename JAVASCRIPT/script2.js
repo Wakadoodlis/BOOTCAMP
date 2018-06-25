c8cf7fd61eae98f7deafce555da3e936ba8e8283
Martynas <marcius117@gmail.com>
2018-06-25 13:07:10 +0300

    exercise

--- JAVASCRIPT/script2.js
+++ JAVASCRIPT/script2.js
@@ -12,20 +12,26 @@
 
 //   SUPER BONUS: if age is a perfect square, print "perfect square!"( sperfect square is 4, 16 )
 alert("in this exercise script2.js is linked. to go in 'how long u living app'link script.js from js folder");
-var age = prompt("What is Your age? ");
+var age = Number(prompt("What is Your age? "));
 
+// if age is negative
 if(age<0) {
   console.log("Error, You are not born Yet.");
 } 
+// If age is 21  
 else if(age==21) {
   console.log("Happy 21rst birthday!! ");
 } 
+// If age is odd
+//(not evenly divisible by two)
 else if(age % 2 ) {
   console.log("Your age is odd");
 }
+// perfect square
 else if(age % Math.sqrt(age)=== 0) {
 console.log("perfect square");
 }
+// normal age
 else {
   console.log (" You are at " +age+ " and it is nice");
 }
