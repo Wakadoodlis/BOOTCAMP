0375c234645442d0383766503026fa295e4c0a4a
Martynas <marcius117@gmail.com>
2018-06-25 14:24:15 +0300

    exercise

--- JAVASCRIPT/js/guess a number app/js/game.js
+++ JAVASCRIPT/js/guess a number app/js/game.js
@@ -1,20 +1,20 @@
 alert("This is 'Gues a number game'. You have to gues a number from 0 to 10. If You succeed, you will feel satisfaction :) ")
 
-// create a number
-var correctNumber = 9;
+// sukuriam skaiciu
+var correctNumber = 6;
 // ask user for a guess
-var number = Number(prompt( "Guess a number! "));
-
+// var number = Number(prompt( "Guess a number! ")); (galima ir taip bet padarom paprasciau)
+var stringNumber = prompt( "Guess a number! ");
+var number = Number(stringNumber);
 // check if user guess it
-if(number<correctNumber) {
-  alert("Your number " +number+ " to low. Try again");
+if(number===correctNumber) {
+  alert("YOU WON! It is a correct number");
 }
+// IF number to big:
 else if(number>correctNumber){
   alert("Your number " +number+ " to big. Try again"); 
 }
-else if(number===correctNumber) {
-  alert( "You have won! It is a correct number");
-}
-while (number<correctNumber || number>correctNumber) {
-  
+// IF number to low
+else {
+  alert( "Your number " +number+ " to low. Try again");
 }
