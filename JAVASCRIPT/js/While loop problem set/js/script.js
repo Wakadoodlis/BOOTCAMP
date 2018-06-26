ddf39c49fe9b0246254b3dc110ab6b9cbbe73c16
Martynas <marcius117@gmail.com>
2018-06-26 13:45:04 +0300

    js exercise

---
+++ JAVASCRIPT/js/While loop problem set/js/script.js
@@ -0,0 +1,34 @@
+// Print all numbers between -10 and 19
+console.log("Print all numbers between -10 and 19");
+
+var num = -10;
+while(num <= 19){
+  console.log(num);
+  num++;
+}
+// Print all even numbers between 10 and 40
+console.log("Print all even numbers between 10 and 40");
+
+var num = 10;
+while(num <= 40) {
+  console.log(num);
+  num+=2
+}
+
+// Print all odd numbers between 300 and 333
+console.log("Print all odd numbers between 300 and 333");
+// var num = 300;
+// while(300 % 2 <= 333) {
+//   console.log(num);
+//   num++;
+// }
+
+// Print all numbers divisible by 5 and 3 between 5 and 50
+
+console.log("Print all numbers divisible by 5 and 3 between 5 and 50");
+
+var num = 5;
+
+while(num /3 && num /5 <= 50){
+  console.log (num);
+}
