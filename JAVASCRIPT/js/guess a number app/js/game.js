9c640ec53cd23c1a97b67969c7c658b16a9a3386
Martynas <marcius117@gmail.com>
2018-06-27 12:47:14 +0300

    loop to the game

--- JAVASCRIPT/js/guess a number app/js/game.js
+++ JAVASCRIPT/js/guess a number app/js/game.js
@@ -2,49 +2,27 @@ alert("This is 'Gues a number game'. You have to gues a number from 0 to 10. If 
 
 // sukuriam skaiciu
 var correctNumber = 6;
-// ask user for a guess
-// var number = Number(prompt( "Guess a number! ")); (galima ir taip bet padarom paprasciau)
+
+// vartotojas speja
 var stringNumber = prompt( "Guess a number! ");
 var number = Number(stringNumber);
-// check if user guess it
-// if(number===correctNumber) {
-//   alert("YOU WON! It is a correct number");
-// }
-// IF number to big loop:
+
+// while loop
 while(number>correctNumber || number<correctNumber ){
+  // jei pasirinktas skaicius didesnis
   if(number>correctNumber) {
     alert("Your number " +number+ " to big. Try again")
 }
-  
-
-  if(number<correctNumber) {
+// jei pasirinktas skaicius mazesnis
+  else if(number<correctNumber) {
     alert("Your number " +number+ " to low. Try again")
   }
 
+  var stringNumber = prompt( "Guess a number! ")
+  var number = Number(stringNumber);
+  // jei pasirinktas skaicius sutampa
   if(number===correctNumber) {
   alert("YOU WON! It is a correct number");
 }
-
-  var stringNumber = prompt( "Guess a number! ")
-  var number = Number(stringNumber); 
+  
 }
-
-
-
-// IF number to low loop
-while(number<correctNumber){
-  alert("Your number " +number+ " to low. Try again")
-  var stringNumber = prompt( "Guess a number! ")
-  var number = Number(stringNumber); 
-  if(number===correctNumber) {
-    alert("YOU WON! It is a correct number");
-  }
-}
-
-
-// while(num <= 40) {
-//   if(num % 2 === 0){
-//   console.log(num);
-// }
-//   num+=1
-// }
