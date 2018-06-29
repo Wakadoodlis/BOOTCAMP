18b58f42fab4b17e1667e34980ea5bd4817df1d4
Martynas <marcius117@gmail.com>
2018-06-29 13:23:12 +0300

    indexOf

--- JAVASCRIPT/js/annoy-auto/annoy.js
+++ JAVASCRIPT/js/annoy-auto/annoy.js
@@ -1,8 +1,18 @@
 // Question
 
+// var ask = prompt("Are we here yet?");
+// // loop
+// while(ask !== "yes" && ask !== "yach" ) {
+//   var ask = prompt("Are we here yet?");
+// }
+// alert("Yes, we finally here!");
+
+
+// VERSION 2
+
 var ask = prompt("Are we here yet?");
-// loop
-while(ask !== "yes" ) {
+// loop with indexOf()
+while(ask.indexOf("yes") === -1) {
   var ask = prompt("Are we here yet?");
 }
 alert("Yes, we finally here!");
