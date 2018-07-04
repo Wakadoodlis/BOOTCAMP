0e04b65e66fced56d875694dd7c4682a66389813
Martynas <marcius117@gmail.com>
2018-07-04 14:00:04 +0300

    js exerc

--- JAVASCRIPT/js/js -even, factorial,kebabtosnake/script.js
+++ JAVASCRIPT/js/js -even, factorial,kebabtosnake/script.js
@@ -1,27 +1,50 @@
 // IF EVEN()
 
+// function ifEven(num) {
+//   // if even then it is true
+//   if (num % 2 === 0) {
+//     return true;
+//   } 
+//   // if not even then it is false
+//   else {
+//     return false;
+//   }
+// }
+
+// ----OOOORRRRR ---
+
 function ifEven(num) {
-  if (num % 2 ===0) {
-    return true;
-  } else
-    return false;
+  return num % 2 === 0;
 }
 
 
-// FACTORIAL ()
+// -----=====  FACTORIAL ()====------- 
 
-var f = [];
-function factorial (n) {
-  if (n == 0 || n == 1)
-    return 1;
-  if (f[n] > 0)
-    return f[n];
-  return f[n] = factorial(n-1) * n;
+// var f = [];
+// function factorial (n) {
+//   if (n == 0 || n == 1)
+//     return 1;
+//   if (f[n] > 0)
+//     return f[n];
+//   return f[n] = factorial(n-1) * n;
+// }
+
+// ---- or ----
+
+function factorial (num){
+  // define the result variable
+  var result = 1;
+  // culculate
+  for(var i = 2; i <= num; i++);
+  result *= i;
+  // returning
+  return result;
 }
 
 
-// KEBAB TO SNAKE ()
+// -----=====  kebab to snake()====------- 
 
 function kebabToSnake(str) {
-  return str.replace(/-/g, "_");  
+  var newStr = str.replace(/-/g, "_");  
+  return newStr;
 }
