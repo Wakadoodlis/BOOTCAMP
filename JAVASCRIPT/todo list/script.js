4bc388ba7fef294e74f3b7c4c8c225993b0c6480
Martynas <marcius117@gmail.com>
2018-07-11 14:28:22 +0300

    todo

--- JAVASCRIPT/todo list/script.js
+++ JAVASCRIPT/todo list/script.js
@@ -1,23 +1,29 @@
 // create an array 
 
-var todos = [];
+window.setTimeout(function() {
+  var todos = [];
 
-// ask user for input
-var input =prompt("What would you like me to do?");
-
-// check what input is: if input is list - do one thing, is input is new - do another thing.
-while(input !== "quit") {
-  // handling inputs
-  if(input === "list") {
-  console.log(todos);
-  } else if(input === "new") {
-  // as user for new todo
-  var newTodo = prompt("Enter new 'Todo' ")
-  // add the new todo to the todos array
-  todos.push(newTodo);
+  // ask user for input
+  var input =prompt("What would you like me to do?");
+  
+  // check what input is: if input is list - do one thing, is input is new - do another thing.
+  while(input !== "quit") {
+    // handling inputs
+    if(input === "list") {
+      alert(todos);
+    } else if(input === "new") {
+      // as user for new todo
+      var newTodo = prompt("Enter new 'Todo' ")
+      // add the new todo to the todos array
+      todos.push(newTodo);
+    } 
+    if(todos == ""){
+      alert ("Your list is empty")
+    }
+    // ask user again
+    input = prompt("What would you like me to do?");
   }
-  // ask user again
-  input = prompt("What would you like me to do?");
-}
-// if while loop is false
-console.log( "You just quit the 'Todo' app");
+  // if while loop is false
+  alert( "You just quit the 'Todo' app");
+
+}, 1000);
