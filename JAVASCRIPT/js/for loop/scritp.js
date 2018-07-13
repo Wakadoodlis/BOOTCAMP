df6430673cdec052518316d091ccb1a414c5778a
Martynas <marcius117@gmail.com>
2018-07-13 13:21:21 +0300

    for

---
+++ JAVASCRIPT/js/for loop/scritp.js
@@ -0,0 +1,10 @@
+// variables
+
+// var num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// var col = ["red", "yellow", "green", "brown"];
+
+for(var num = 0; num <= 50; num++) {
+  if(num % 3 === 0){
+    console.log(num); 
+  }
+}
