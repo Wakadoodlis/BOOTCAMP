43db8492be5cb9bc169760ec60440d597698d79f
Martynas <marcius117@gmail.com>
2018-07-13 14:20:18 +0300

    todo

--- JAVASCRIPT/todo list/script.js
+++ JAVASCRIPT/todo list/script.js
@@ -5,7 +5,7 @@
 
 window.setTimeout(function() {
   // creating an array 
-  var todos = [];
+  var todos = ["love Gintare every single day"];
 
   // ask user for input
   var input =prompt("What would you like me to do?");
@@ -14,17 +14,13 @@ window.setTimeout(function() {
   while(input !== "quit") {
     // handling inputs
     if(input === "list") {
-      alert(todos);
+      listTodos();
     } else if(input === "new") {
-      // as user for new todo
-      var newTodo = prompt("Enter new 'Todo' ")
-      // add the new todo to the todos array
-      todos.push(newTodo);
-    } else if(input !== "list" && input !== "new") {
-      alert("In order to use this app, please use 'new', 'list' or 'quit' functions from the instruction ")
-    } 
-    if(todos == ""){
-      alert ("Your list is empty")
+      newTodos();
+    }  else if (input === "del") {
+      delTodos();
+    } else if(input !== "list" && input !== "new" && input !== "del") {
+      notTodos();
     }
     // ask user again
     input = prompt("What would you like me to do?");
@@ -32,4 +28,33 @@ window.setTimeout(function() {
   // if while loop is false
   alert( "You just quit the 'Todo' app");
 
+
+  function listTodos(){
+    console.log("************");
+    todos.forEach(function(todo, i){
+      console.log(i + ": " + todo);
+    });
+    console.log("************");
+  }
+  
+  function newTodos() {
+    // ask user for new todo
+     var newTodo = prompt("Enter new 'Todo' ")
+    // add the new todo to the todos array
+    todos.push(newTodo);
+    console.log("Added new Todo")
+  }
+  
+  function notTodos() {
+    alert("In order to use this app, please use 'new', 'list' 'del' or 'quit' functions from the instruction ")
+  }
+  
+  function delTodos() {
+    var index = prompt("Enter index of Todo, that You want to delete.")
+    todos.splice(index, 1);
+    alert("Todo of index " + index + " was deleted.")
+  }
+
 }, 1000);
+
+
