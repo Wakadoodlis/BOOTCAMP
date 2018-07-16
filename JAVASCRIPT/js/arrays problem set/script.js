da4b05ca1a66db4d2db4e0a7cec4e27693cae912
Martynas <marcius117@gmail.com>
2018-07-16 18:17:34 +0300

    rev

--- JAVASCRIPT/js/arrays problem set/script.js
+++ JAVASCRIPT/js/arrays problem set/script.js
@@ -12,17 +12,6 @@
 
 
 
-// var fruits = ["Banana", "Orange", "Apple", "Mango"];
-
-
-// function myFunction() {
-//     fruits.forEach(function(name){
-//       console.log(name.reverse);
-//     });
-    
-// }
-
-
 
 
 var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
