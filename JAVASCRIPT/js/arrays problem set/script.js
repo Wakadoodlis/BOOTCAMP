2128d148b2f18fb8d2da3206ee5c7527ada504b5
Martynas <marcius117@gmail.com>
2018-07-16 18:36:32 +0300

    ar

--- JAVASCRIPT/js/arrays problem set/script.js
+++ JAVASCRIPT/js/arrays problem set/script.js
@@ -2,27 +2,27 @@
 
 // var nums = [1,2,3,4,5,6];
 
-// function printReverse() {
-//   var nums = [1,2,3,4,5,6];
-//   nums.forEach(function(nums){
-//     nums.reverse();
-//   });
-//   console.log(nums)
-// }
+function printReverse() {
+  var nums = [1,2,3,4,5,6];
+  nums.forEach(function(nums){
+    console.log(nums.reverse);
+  });
+  
+}
 
 
 
 
 
-var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-var reverse1 = function() {
-  var reversed = array.slice().reverse();
-};
+// var reverse1 = function() {
+//   var reversed = array.slice().reverse();
+// };
 
-var reverse2 = function() {
-  var reversed = [];
-  for (var i = array.length - 1; i >= 0; i--) {
-    reversed.push(array[i]);
-  }
-};
+// var reverse2 = function() {
+//   var reversed = [];
+//   for (var i = array.length - 1; i >= 0; i--) {
+//     reversed.push(array[i]);
+//   }
+// };
