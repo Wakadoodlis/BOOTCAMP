b87648879ec895267e3fdf493c602ec09dcf0578
Martynas <marcius117@gmail.com>
2018-07-24 15:54:52 +0300

    arr

--- JAVASCRIPT/js/arrays problem set/script.js
+++ JAVASCRIPT/js/arrays problem set/script.js
@@ -5,3 +5,16 @@ for(var i = arr.length - 1; i >=0; i--) {
   console.log(arr[i]);
 }
 }
+
+
+// ===--isUNIFORM()=====--- - ITS PRInts true or false. True, when all units in array are the same, and false, when it is not. exmp [1,1,1,1] // true and [1,1,1,3] - false.
+
+function isUniform(arr) {
+  var seka = arr[0];
+  for (var i = 1; i > arr.length; i++) {
+    if( arr[i] !== seka) {
+      return false;
+    }
+  }
+  return true;
+}
